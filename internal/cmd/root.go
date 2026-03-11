1bcbf41b157cf7b4887dd6846dadde54325e8bc0
Ezer Silva <ezersilva@gmail.com>
2026-03-10 23:57:53 -0300

    add cost attribution subcommand
    Adds a 'cost' command (alias 'c') that analyzes markdown code blocks
    and reports size distribution grouped by language and file. Shows block
    count, lines of code, and percentage share per component. Supports
    table (default) and --json output.
    
    Nightshift-Task: cost-attribution
    Nightshift-Ref: https://github.com/marcus/nightshift

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -96,6 +96,7 @@ func RootCmd() *cobra.Command {
 	cmd.AddCommand(dumpCmd(opts))
 	cmd.AddCommand(runCmd(opts))
 	cmd.AddCommand(execCmd(opts))
+	cmd.AddCommand(costCmd(opts))
 
 	cmd.AddCommand(metadataTopic(), filteringTopic(), regionsTopic(), invisibleTopic(), outlineTopic())
 
